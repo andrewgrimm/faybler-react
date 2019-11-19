5c4da9d65a5605cbea280a8b1ca48e4dd4c431c3
Andrew Grimm <andrewgrimm2@gmail.com>
2019-11-19 11:31:51 +1100

    FAYB-12-Create-featured-page - Completed further refactoring of package.json so that code is now able to compile when npm install --production is used to install the dependencies. Fixed small issue caused when using webpack.merge on multiple webpack config files. Changed config so that now calls to the express server from the front end are proxied to backend by webpack-dev-server to prevent issues with CORS ocurring when the front end and backend are running on different ports of localhost.

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,16 +5,38 @@ const common = require('./webpack.common.js');
 
 const distPath = path.join(__dirname, 'dist');
 
-module.exports = merge(common, {
+module.exports = merge.smart(common, {
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts)?$/,
+        exclude: /node_modules/,
+        use: ['eslint-loader'],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        resolve: { extensions: ['.js', '.jsx'] },
+        exclude: /node_modules/,
+        use: ['eslint-loader'],
+
+      }
+    ],
+  },
   plugins: [
-    new EnvironmentPlugin({ HOST: 'localhost:9000' }),
+    new EnvironmentPlugin({ HOST: 'localhost:8000' }),
   ],
   devServer: {
     contentBase: distPath,
     historyApiFallback: true,
     compress: true,
     port: 8000,
+    proxy: {
+      '**': 'http://localhost:9000',
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
   },
   devtool: 'cheap-eval-source-map',
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,8 @@ const merge = require('webpack-merge');
 const { EnvironmentPlugin } = require('webpack');
 const common = require('./webpack.common.js');
 
-// this is used for the deployment to both the dev server and prod server but not for running on localhost.
+/* This is used for the deployment to both the dev server
+   and prod server but not for running on localhost. */
 
 module.exports = merge(common, {
   mode: 'production',
